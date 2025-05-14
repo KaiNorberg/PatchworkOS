b6fc5abac546e45080564d76d00b4fc3da210a93
KN <KN.KaiNorberg@gmail.com>
2025-05-14 17:27:19 +0200

    top: bug fix

--- src/programs/top/main.c
+++ src/programs/top/main.c
@@ -60,7 +60,7 @@ int main(void)
         {
             percentage = ((double)(activeDelta + trapDelta) * 100.0) / (double)totalDelta;
         }
-        printf("cpu%d %d.%3d%% usage\n", before[i].id, (int)percentage, (int)(percentage * 100.0));
+        printf("cpu%d %d.%03d%% usage\n", before[i].id, (int)percentage, (int)(percentage * 100.0));
     }
 
     return 0;
