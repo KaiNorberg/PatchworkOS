343b3c2f348e0afba0cc974ee5e447381acd7860
KN <KN.KaiNorberg@gmail.com>
2025-05-21 15:21:00 +0200

    libstd: small clock_t edit

--- include/libstd/_AUX/clock_t.h
+++ include/libstd/_AUX/clock_t.h
@@ -3,8 +3,8 @@
 
 // We use clock_t to represent any nanosecond time.
 
-typedef unsigned long long clock_t;
-#define CLOCKS_PER_SEC ((clock_t)1000000000) // Nanoseconds per second
-#define CLOCKS_NEVER (__UINT64_MAX__)        // Extension
+typedef __UINT64_TYPE__ clock_t;
+#define CLOCKS_PER_SEC ((clock_t)1000000000UL) // Nanoseconds per second
+#define CLOCKS_NEVER ((clock_t)__UINT64_MAX__)        // Extension
 
 #endif
