a9f3bbaf4dc8e6b8c88b0fc047afcb8ed4fd4880
KN <KN.KaiNorberg@gmail.com>
2026-01-14 16:13:00 +0100

    fix(boot): inline elf64_load_segments

--- src/boot/main.c
+++ src/boot/main.c
@@ -14,7 +14,6 @@
 // Include functions directly to avoid multiple object files
 #include <functions/assert/assert.c>
 #include <functions/elf/elf64_get_loadable_bounds.c>
-#include <functions/elf/elf64_load_segments.c>
 #include <functions/elf/elf64_validate.c>
 
 /**
@@ -875,7 +874,27 @@ static EFI_STATUS kernel_load(boot_kernel_t* kernel, EFI_FILE* rootHandle)
     }
 
     Print(L"  Loading kernel...\n");
-    elf64_load_segments(&kernel->elf, (Elf64_Addr)kernelPhys, minVaddr);
+    Elf64_Ehdr* header = (Elf64_Ehdr*)kernel->elf.header;
+    for (uint32_t i = 0; i < header->e_phnum; i++)
+    {
+        Elf64_Phdr* phdr = ELF64_GET_PHDR(&kernel->elf, i);
+        if (phdr->p_type != PT_LOAD)
+        {
+            continue;
+        }
+
+        uint8_t* dest = (uint8_t*)(kernelPhys + (phdr->p_vaddr - minVaddr));
+        uint8_t* src = ELF64_AT_OFFSET(&kernel->elf, phdr->p_offset);
+        uint64_t j = 0;
+        for (; j < phdr->p_filesz; j++)
+        {
+            dest[j] = src[j];
+        }
+        for (; j < phdr->p_memsz; j++)
+        {
+            dest[j] = 0;
+        }
+    }
     kernel->physAddr = (void*)kernelPhys;
 
     Print(L"  Entry Code: ");
